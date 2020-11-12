db74bac824d9b6996ede1f092a315de79638bdda
MaisarahPauzi <maisarah.mohamed.pauzi@gmail.com>
2020-11-12 23:13:08 +0800

    configure whitenoise

--- priceapi/settings.py
+++ priceapi/settings.py
@@ -31,6 +31,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'priceapi.urls'
@@ -97,3 +98,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
