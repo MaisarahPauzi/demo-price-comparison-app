a1b5aafc2ee680580e290480bfb06af22e744722
MaisarahPauzi <maisarah.mohamed.pauzi@gmail.com>
2020-11-12 23:16:37 +0800

    configure whitenoise

--- priceapi/settings.py
+++ priceapi/settings.py
@@ -98,4 +98,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
